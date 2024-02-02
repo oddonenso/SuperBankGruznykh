dfae7b8140676af2aaa8b5c52724ee77dc40155b
oddonenso <oddonenso@gmail.com>
2024-02-02 17:29:28 +0700

    СуперБанк

---
+++ wpfBank/Pages/Calculator.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace wpfBank.Pages
+{
+    public partial class Calculator : Page
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParam_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Получаем введенные значения
+                double sum = Convert.ToDouble(tbsum.Text);
+                double percent = Convert.ToDouble(tbsrok.Text);
+                int months = Convert.ToInt32(tbpopolnit.Text);
+
+                // Рассчитываем доход
+                double rate = percent / 100 / 12; // Процентная ставка в месяц
+                double futureValue = sum * Math.Pow(1 + rate, months); // Формула для ежемесячной капитализации
+
+                // Выводим результат на интерфейс
+                tbDohodStab.Text = futureValue.ToString("C2");
+                tbDohodOptimal.Text = (futureValue - sum).ToString("C2");
+                tbDohodStandart.Text = ((futureValue - sum) / months).ToString("C2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при расчете: " + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ wpfBank/Pages/Log.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfBank.Pages
+{
+
+    public partial class Log : Page
+    {
+        private User currentUser;
+        public Log(User currentUser)
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Deposist deposist = new Deposist();
+            this.NavigationService.Navigate(deposist);
+        }
+        private void Tarif_Click(object sender, RoutedEventArgs e)
+        {
+            Tariff tariffPage = new Tariff();
+            this.NavigationService.Navigate(tariffPage);
+        }
+    }
+}
